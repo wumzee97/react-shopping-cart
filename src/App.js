8897d69549eccdc1dd9b1713c0ad440fca6bf1dc
mac <adewumi.alabi@fundall.io>
2020-10-18 17:10:35 +0100

    Products component updates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-// feature1
-function App() {
-  return (
-    <div className="grid-container">
-      <header >
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All rights reserved.
-      </footer>
-    </div>
-  );
+import React from "react";
+import Products from "./components/Products";
+import data from "./data.json";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All rights reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
